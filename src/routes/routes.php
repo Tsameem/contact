28ab20dd69a10dd920da6c85f7b22fa3aad773c3
Tsameem <khaled.mqd@gmail.com>
2017-07-21 03:54:38 -0700

    Update routes.php

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -5,5 +5,5 @@ Route::group(['middleware' => 'web'], function() {
 
 	// Client's routes
 	Route::get('contact-us', '\App\Http\Controllers\Client\ContactsController@index');
-	Route::post('contact-us', '\App\Http\Controllers\Client\ContactsController@store');
+	Route::post('contact-us', '\App\Http\Controllers\Client\ContactsController@store')->name('contact_store');
 });
