32495d051bfdd9ccfa56edeaea94522541f5e473
Tsameem <khaled.mqd@gmail.com>
2017-07-21 05:13:00 -0700

    Update ContactsController.php

--- src/controllers/Admin/ContactsController.php
+++ src/controllers/Admin/ContactsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth:admin');
+	}
 	public function index()
 	{
 		return view('tsameem.contact.admin.contact_us');
